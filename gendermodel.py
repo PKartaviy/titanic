33a7235b359ec2504325a9217bc2075dd4a23eb3
Pavel Kartavyy <p.kartaviy@gmail.com>
2013-02-22 02:37:30 +0400

    Improved gendermodel. My score is 0.69378

--- gendermodel.py
+++ gendermodel.py
@@ -7,6 +7,66 @@ Date : 18th September, 2012
 
 import csv as csv
 import numpy as np
+import operator
+
+id = { 'pclass': 1, 'sex': 3, 'age': 4, 'sibsp':  5, 'parch': 6, 'embarked': 10}
+max = {'pclass': 3, 'sex': 2, 'age': 8, 'sibsp':  9, 'parch': 10, 'embarked': 3}
+
+def bakeData(data):
+    rows = np.size(data[0::, 0])
+    columns = np.size(data[0, 0::])
+    #transform data to equal form
+    data[data[0::, id['pclass']]=='1', id['pclass']] = 0
+    data[data[0::, id['pclass']]=='2', id['pclass']] = 1
+    data[data[0::, id['pclass']]=='3', id['pclass']] = 2
+    
+    data[data[0::, id['sex']]=='male', id['sex'] ] = 1
+    data[data[0::, id['sex']]=='female', id['sex'] ] = 0
+    
+    data[ data[0::, id['embarked']]=='S', id['embarked'] ] = 0
+    data[ data[0::, id['embarked']]=='C', id['embarked'] ] = 1
+    data[ data[0::, id['embarked']]=='Q', id['embarked'] ] = 2
+    data[ data[0::, id['embarked']]=='', id['embarked'] ] = 0
+    
+    incorAge = data[0::, id['age']]==''
+    corAge = data[0::, id['age']]!=''
+    data[incorAge, id['age']] = np.median(data[corAge, id['age']].astype(np.float) )
+    for k in range(rows):
+        data[k, id['age']] = int(float(data[k, id['age']]))/12
+    
+def calcEntropy(data):    
+    weights = {}
+    number_passengers = np.size(data[0::,0])
+    for key in id :
+        # cast all arrays to float
+        dataColumn = data[0::, id[key]].astype(np.float)
+        weights[key] = []
+        for value in range(max[key]):
+            passengersWithValue = dataColumn == value
+            numPassengersWithValue = np.size(data[passengersWithValue, id[key]])
+            survivedPassengersWithValue = np.sum(data[passengersWithValue, 0 ].astype(np.float)) 
+            survivingRate = 0.5
+            # Ignore statistic which has not enough data
+            if(numPassengersWithValue>5):
+                survivingRate = float(survivedPassengersWithValue)/numPassengersWithValue
+            weights[key].append( survivingRate)
+
+    for key in weights.keys():
+        print key, weights[key]
+    
+    return weights
+
+def calcProbability(row, weights):
+    #just calc as average sum
+    probability = 0.0
+    n = 0
+    for key in id :
+        n += 1
+        col = id[key]
+        probability += weights[key][ int(row[col]) ]
+    probability = probability / n
+    
+    return probability
 
 csv_file_object = csv.reader(open('csv/train.csv', 'rb')) #Load in the csv file
 header = csv_file_object.next() #Skip the fist line as it is a header
@@ -21,13 +81,16 @@ data = np.array(data) #Then convert from a list to an array
 #I have to add the astype command
 #as when reading in it thought it was  a string so needed to convert
 
+bakeData(data)
+weights = calcEntropy(data)
+
 number_passengers = np.size(data[0::,0].astype(np.float))
 number_survived = np.sum(data[0::,0].astype(np.float))
 proportion_survivors = number_passengers / number_survived
 
 # I can now find the stats of all the women on board
-women_only_stats = data[0::,3] == "female" #This finds where all the women are
-men_only_stats = data[0::,3] != "female" #This finds where all the men are
+women_only_stats = data[0::,3] == '0' #This finds where all the women are
+men_only_stats = data[0::,3] != '0' #This finds where all the men are
                                          # != means not equal
 
 #I can now find for example the ages of all the women by just placing
@@ -54,11 +117,18 @@ header = test_file_obect.next()
 
 open_file_object = csv.writer(open("csv/genderbasedmodelpy.csv", "wb"))
 
+test_data = []
 for row in test_file_obect:
-    if row[2] == 'female':
-        row.insert(0,'1') #Insert the prediciton at the start of the row
+    row.insert(0,'7')
+    test_data.append(row)
+test_data = np.array(test_data)
+bakeData(test_data)
+
+for row in test_data:
+    if calcProbability(row, weights) > 0.5 :
+        row[0] = '1' #Insert the prediciton at the start of the row
         open_file_object.writerow(row) #Write the row to the file
     else:
-        row.insert(0,'0')
+        row[0] = '0'
         open_file_object.writerow(row)
 
